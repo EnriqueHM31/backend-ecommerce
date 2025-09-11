68fedf828d2b458e891eadf252de2f7e791dff84
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-09-11 17:09:37 -0600

    Update .gitignore to include .dist and .build directories. Refactor import paths in pedidos controller for consistency, changing from absolute to relative paths for utility imports.

--- src/controllers/pedidos.ts
+++ src/controllers/pedidos.ts
@@ -1,9 +1,9 @@
-import { CheckearCompra, CheckearProducto, CrearCompra, disminuirStock, InsertarItems, ModificarEstado, obtenerCompras, restaurarStock } from '@/utils/consultas/compras';
-import { CheckearUsuario } from '@/utils/consultas/Usuario';
-import { CartItemsValidation } from '@/utils/validaciones/CartItems';
+import { CheckearCompra, CheckearProducto, CrearCompra, disminuirStock, InsertarItems, ModificarEstado, obtenerCompras, restaurarStock } from '../utils/consultas/compras';
+import { CheckearUsuario } from '../utils/consultas/Usuario';
+import { CartItemsValidation } from '../utils/validaciones/CartItems';
 import { Request, Response } from 'express';
 import { db } from '../database/db';
-import { UsuarioValidation } from '@/utils/validaciones/usuario';
+import { UsuarioValidation } from '../utils/validaciones/usuario';
 
 export class pedidosController {
     static async crearPedido(req: Request, res: Response) {
