62fd66c1531c9ce2be49abb06aa6ce55befbd65a
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-09-11 17:44:47 -0600

    pasos

--- build/routes/pagos.routes.js
+++ build/routes/pagos.routes.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CompraRouter = void 0;
 // routes/compra.ts
-const compra_1 = require("@/controllers/compra");
+const compra_1 = require("../controllers/compra");
 const express_1 = require("express");
 exports.CompraRouter = (0, express_1.Router)();
 exports.CompraRouter.post("/checkout-session", compra_1.CompraController.RealizarCompra);
