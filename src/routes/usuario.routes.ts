87a88bc70a451410383a2466a65a0c2a9ad340a4
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-09-10 19:30:49 -0600

    Implement AutenticacionController for user authentication and refactor usuario.routes.ts to utilize the new controller. This change centralizes authentication logic, enhances error handling, and improves code maintainability.

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,53 +1,5 @@
-import { CheckearUsuario, InsertarUsuario } from "@/utils/consultas/Usuario";
-import { UsuarioValidation } from "@/utils/Validaciones/usuario";
+import { AutenticacionController } from "@/controllers/autenticacion";
 import { Router } from "express";
 export const UsuarioRouter = Router();
 
-UsuarioRouter.post("/auth", async (req, res) => {
-    try {
-        const { usuario_id, nombre, correo, avatar } = req.body;
-
-        // Validación con Zod
-        const resultadoValidarUsuario = UsuarioValidation.RevisarUsuario({ usuario_id, nombre, correo, avatar });
-
-        if (!resultadoValidarUsuario.success) {
-            res.status(400).json({
-                creado: false,
-                success: false,
-                message: resultadoValidarUsuario.error.issues.map(err => err.message).join(", ")
-            });
-        }
-
-        // Revisar si ya existe el usuario
-        const { existe } = await CheckearUsuario(usuario_id);
-
-        if (!existe) {
-            console.log('Creando usuario...');
-            await InsertarUsuario(usuario_id, nombre, correo, avatar);
-
-            res.status(203).json({
-                success: true,
-                creado: true,
-                message: 'Registro exitoso',
-                data: { usuario_id, nombre, correo, avatar }
-            });
-        } else {
-            res.status(203).json({
-                success: true,
-                creado: false,
-                message: 'Usuario ya registrado',
-                data: { usuario_id, nombre, correo, avatar }
-            });
-        }
-
-
-    } catch (error) {
-        console.error("Error en /auth:", error);
-        res.status(500).json({
-            success: false,
-            message: error instanceof Error ? error.message : "Error al procesar la autenticación",
-            data: null,
-            creado: false
-        });
-    }
-});
+UsuarioRouter.post("/auth", AutenticacionController.Auth);
